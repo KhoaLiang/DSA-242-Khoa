39ec77751fb803e3a3c796905304fd05cd345ddf
KhoaLiang <akhoa2020@gmail.com>
2025-03-03 21:18:22 +0700

    done lab1

--- Lab_L03/Lab1/SLinkedList_SLinkedListADT.cpp
+++ Lab_L03/Lab1/SLinkedList_SLinkedListADT.cpp
@@ -32,9 +32,12 @@ public:
     string toString();
     bool    empty();
     T       get(int index);
-    void    set(int index, T e);
-    int     indexOf(T item);
-    bool    contains(T item);
+    void    set(int index, const T& e);
+    int     indexOf(const T& item);
+    bool    contains(const T& item);
+    T       removeAt(int index);
+    bool    removeItem(const T& item);
+    void    clear();
 public:
     class Node {
     private:
@@ -167,6 +170,157 @@ int SLinkedList<T>::size() {
     /* Return the length (size) of list */ 
     return count;
 }
+
+template<class T>
+T SLinkedList<T>::get(int index) {
+    /* Give the data of the element at given index in the list. */
+    if (index < 0 || index >= count) {
+        throw std::out_of_range("index is out of range");
+    }
+    Node* current = head;
+    for (int i = 0; i < index; i++) {
+        current = current->next;
+    }
+    return current->data;
+}
+
+template <class T>
+void SLinkedList<T>::set(int index, const T& e) {
+    /* Assign new value for element at given index in the list */
+    if (index < 0 || index >= count) {
+        throw std::out_of_range("index is out of range");
+    }
+    Node* current = head;
+    for (int i = 0; i < index; i++) {
+        current = current->next;
+    }
+    current->data = e;
+}
+
+template<class T>
+bool SLinkedList<T>::empty() {
+    /* Check if the list is empty or not. */
+    if (head != nullptr)
+    {
+        return false;
+    }
+    return true;
+}
+
+template<class T>
+int SLinkedList<T>::indexOf(const T& item) {
+    /* Return the first index whether item appears in list, otherwise return -1 */
+    Node* current = head;
+    int currentIndex = 0;
+    while (current != nullptr) {
+        if (current->data == item) {
+            return currentIndex;
+        }
+        currentIndex++;
+        current = current->next;
+    }
+    return -1;
+}
+
+template<class T>
+bool SLinkedList<T>::contains(const T& item) {
+    /* Check if item appears in the list */
+    Node* current = head;
+    while (current != nullptr)
+    {
+        if (current->data == item)
+        {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+template <class T>
+T SLinkedList<T>::removeAt(int index) {
+    /* Remove element at index and return removed value */
+    if (index < 0 || index >= count) {
+        throw std::out_of_range("index is out of range");
+    }
+
+    Node* prev = nullptr;
+    Node* current = head;
+
+    // Traverse to the node at the given index
+    for (int i = 0; i < index; i++) {
+        prev = current;
+        current = current->next;
+    }
+
+    // If removing the head element
+    if (prev == nullptr) {
+        head = current->next;
+    } else {
+        prev->next = current->next;
+    }
+
+    // If removing the tail element
+    if (current->next == nullptr) {
+        tail = prev;
+    }
+
+    T deleteData = current->data;
+    delete current;
+    count--;
+
+    return deleteData;
+}
+
+template <class T>
+bool SLinkedList<T>::removeItem(const T& item) {
+    /* Remove the first appearance of item in list and return true, otherwise return false */
+    Node* prev = nullptr;
+    Node* current = head;
+
+    // Traverse to find the node with the given item
+    while (current != nullptr && current->data != item) {
+        prev = current;
+        current = current->next;
+    }
+
+    // If the item is not found
+    if (current == nullptr) {
+        return false;
+    }
+
+    // If removing the head element
+    if (prev == nullptr) {
+        head = current->next;
+    } else {
+        prev->next = current->next;
+    }
+
+    // If removing the tail element
+    if (current->next == nullptr) {
+        tail = prev;
+    }
+
+    delete current;
+    count--;
+
+    return true;
+}
+
+template<class T>
+void SLinkedList<T>::clear() {
+    /* Remove all elements in list */
+    Node* current = head;
+    while (current != nullptr) {
+        Node* nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+}
+
 
 int main() {
     SLinkedList<int> list;
