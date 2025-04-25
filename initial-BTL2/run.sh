7ca16931094957897c789c685d314d0f1f737704
KhoaLiang <akhoa2020@gmail.com>
2025-04-26 00:23:06 +0700

    FUCH YEAH IT WORKS

--- initial-BTL2/run.sh
+++ initial-BTL2/run.sh
@@ -1,1 +1,7 @@
 g++ -g -I include -I src -std=c++17 src/test/* src/main.cpp -o main && ./main
+
+# A compile that actually works
+g++ -g -I include -I src -std=c++17 src/test/tc_xmap.cpp src/test/tc_heap.cpp src/main.cpp -o main
+
+# Surprisingly, this works too
+g++ -g -I include -I src -std=c++17 src/test/tc_xmap.cpp src/test/tc_heap.cpp src/test/tc_compressor.cpp src/main.cpp -o main
